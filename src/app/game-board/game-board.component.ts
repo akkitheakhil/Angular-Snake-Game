810e43ab7c5e0d3856069db6ee973d759f135a84
akkitheakhil <akhilpad007@gmail.com>
2021-03-24 18:24:04 +0530

    Updated Game Configs

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Food } from '../game-engine/food';
 import { outsideGrid } from '../game-engine/gameboard-grid.util';
-import { Snake, SNAKE_SPEED } from '../game-engine/snake';
+import { Snake } from '../game-engine/snake';
 
 @Component({
   selector: 'app-game-board',
@@ -13,6 +13,7 @@ export class GameBoardComponent implements OnInit, AfterViewInit {
   lastRenderTime = 0
   gameOver = false
   gameBoard: any;
+  SNAKE_SPEED = 1;
   snake = new Snake();
   food = new Food(this.snake);
   constructor() { }
@@ -32,16 +33,21 @@ export class GameBoardComponent implements OnInit, AfterViewInit {
 
     window.requestAnimationFrame(this.start.bind(this));
     const secondsSinceLastRender = (currentTime - this.lastRenderTime) / 1000;
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+    if (secondsSinceLastRender < 1 / this.snakeSpeed) return;
     this.lastRenderTime = currentTime;
     // console.log("rendering");
-
     this.update();
     this.draw();
-
   }
 
 
+  get snakeSpeed() {
+    const score = this.food.currentScore;
+    if(score < 10) return 4;
+    if(score > 10 &&  score < 15 ) return 5;
+    if(score > 15 && score < 20 ) return 6;
+    return 7;
+  }
 
   dpadMovement(direction: string) {
     this.snake.input.setDirection(direction);
